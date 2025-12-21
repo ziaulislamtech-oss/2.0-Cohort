25afe4a4d9ce6db8be5420cbbdd3d3a191171c7a
Zia-Ul-Islam <ziaulislam.tech@gmail.com>
2025-12-21 19:11:24 +0500

    throttleFuncitonDone

--- Day-64(debouncing&throtling/script.js
+++ Day-64(debouncing&throtling/script.js
@@ -1,15 +1,37 @@
-function debounce(fnc,delay){
-    let timer;
+// ----------------Debouncing-----------------
+
+// function debounce(fnc,delay){
+//     let timer;
+//     return function(...args){
+//         clearTimeout(timer);
+//         timer = setTimeout(function(){
+//             fnc(...args);
+//             // console.log('hey')
+//         },delay)
+//     };
+// }
+// let searchBar = document.querySelector('#search');
+// searchBar.addEventListener('input',debounce(function(dets){
+//     console.log(dets)
+// },1000)
+// )
+
+// --------------------------
+
+//--------------Throtling--------------
+
+function throttle(fnc,delay){
+    let timer = 0;
     return function(...args){
-        clearTimeout(timer);
-        timer = setTimeout(function(){
-            fnc(...args);
-            // console.log('hey')
-        },delay)
-    };
+        let now = Date.now();
+        if(now - timer >= delay){
+            timer = now;
+            fnc(...args)
+        }
+    }
 }
-let searchBar = document.querySelector('#search');
-searchBar.addEventListener('input',debounce(function(dets){
-    console.log(dets)
-},1000)
-)
+
+let searchBar = document.querySelector('#search')
+searchBar.addEventListener('input',throttle(function(){
+    console.log('ran')
+},1000))
