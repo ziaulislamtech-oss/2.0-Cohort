c15a15f1d4d3b1b9a0b73e2ac665fb3b8012e6a0
Zia-Ul-Islam <ziaulislam.tech@gmail.com>
2025-12-21 19:28:23 +0500

    justPracticeDebouncingAgain

--- Day-64(debouncing&throtling/script.js
+++ Day-64(debouncing&throtling/script.js
@@ -12,26 +12,58 @@
 // }
 // let searchBar = document.querySelector('#search');
 // searchBar.addEventListener('input',debounce(function(dets){
-//     console.log(dets)
+    //     console.log(dets)
 // },1000)
 // )
 
-// --------------------------
-
-//--------------Throtling--------------
-
-function throttle(fnc,delay){
-    let timer = 0;
+function debounce(fnc,delay){
+    let timer;
     return function(...args){
-        let now = Date.now();
-        if(now - timer >= delay){
-            timer = now;
+        clearTimeout(timer);
+        timer = setTimeout(()=>{
             fnc(...args)
-        }
+        },delay)
     }
 }
-
-let searchBar = document.querySelector('#search')
-searchBar.addEventListener('input',throttle(function(){
-    console.log('ran')
+let searchBar = document.querySelector('#search');
+searchBar.addEventListener('input',debounce(function(){
+    console.log('huihuui')
 },1000))
+
+
+
+// --------------------------
+
+// // --------------Throtling--------------
+
+// function throttle(fnc,delay){
+//     let timer = 0;
+//     return function(...args){
+//         let now = Date.now();
+//         if(now - timer >= delay){
+//             timer = now;
+//             fnc(...args)
+//         }
+//     }
+// }
+
+// let searchBar = document.querySelector('#search')
+// searchBar.addEventListener('input',throttle(function(){
+//         console.log('ran')
+// },1000))
+// function throttle(fnc,delay){
+//     let timer = 0;
+//     return function(...args){
+//         let now = Date.now();
+//         if(now - timer >= delay){
+//             timer = now
+//             fnc(...args)
+
+//         }
+//     }
+// }
+
+// let searchBar = document.querySelector('#search')
+// searchBar.addEventListener('input',throttle(function(){
+//     console.log('hey')
+// },1000))
