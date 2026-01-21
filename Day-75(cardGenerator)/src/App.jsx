626d60eb26084431d623a4faee73b7ecbcd13546
Zia-Ul-Islam <ziaulislam.tech@gmail.com>
2026-01-21 21:11:44 +0500

    Day76CardGeneratorLocalStorage

--- Day-75(cardGenerator)/src/App.jsx
+++ Day-75(cardGenerator)/src/App.jsx
@@ -2,40 +2,53 @@ import React, { useState } from 'react'
 import Card from './Component/Card'
 
 const App = () => {
-  const [name, setname] = useState('')
-  const [url, setUrl] = useState('')
-  const [role, setRole] = useState('')
-  const [description, setDescription] = useState('')
-  const [allUsers,setAllUsers]=useState([])
+  const [userName, setUserName] = useState('')
+  const [userRole, setUserRole] = useState('')
+  const [imageURL, setImageUrl] = useState('')
+  const [userDesc, setUserDesc] = useState('')
+  const localData = JSON.parse(localStorage.getItem('all-users') ) || []
+  console.log(localData)
+
+  const [allUsers,setAllUsers]=useState(localData)
   function submitHandler(e){
     e.preventDefault()
-    const oldUser = [...allUsers]
-    setAllUsers([...oldUser,{name,url,role,description}])
+   const oldUser = [...allUsers]
+   oldUser.push({userName,userRole,userDesc,imageURL})
+   setAllUsers(oldUser)
+   localStorage.setItem('all-users',JSON.stringify(oldUser))
+    
     console.log(allUsers)
+    
 
 
-    setname('')
-    setUrl('')
-    setRole('')
-    setDescription('')
+    setUserName('')
+    setUserRole('')
+    setImageUrl('')
+    setUserDesc('')
   
   }
 function deleteItem(id){
-  console.log('item Deleted')
+
   const copyUsers =[...allUsers]
-  copyUsers.splice(id,1)
-  console.log(copyUsers)
-  setAllUsers(copyUsers)
+  const conf = confirm('Are you sure to delete this card')
+  if(conf){
+    copyUsers.splice(id,1)
+    setAllUsers(copyUsers)
+    localStorage.setItem('all-users',JSON.stringify(copyUsers))
+    
+  }
+  
 
 }
   return (
-    <div className=''>
+    <div className=' p-10'>
+      <h1 className='text-center font-extrabold text-4xl font-serif '>Card Generator</h1>
       <form onSubmit={(e)=> submitHandler(e)} className='flex flex-wrap items-center'>
-        <input value={name} onChange={(e)=> setname(e.target.value)} className='border-2 m-1 px-5 py-2 w-[45%] rounded' type="text" placeholder='Enter your name' />
-        <input value={url} onChange={(e)=> setUrl(e.target.value)} className='border-2 m-1 px-5 py-2 w-[45%] rounded' type="text" placeholder='Image URL' />
-        <input value={role} onChange={(e)=> setRole(e.target.value)} className='border-2 m-1 px-5 py-2 w-[45%] rounded' type="text" placeholder='Enter role' />
-        <input value={description} onChange={(e)=> setDescription(e.target.value)} className='border-2 m-1 px-5 py-2 w-[45%] rounded' type="text" placeholder='Enter Description' />
-        <button className='basis-[90%] px-3 py-3 bg-amber-200'>Submit</button>
+        <input value={userName} onChange={(e)=> setUserName(e.target.value)} className='border-2 m-1 px-5 py-2 w-[45%] rounded' type="text" placeholder='Enter your name' />
+        <input value={imageURL} onChange={(e)=> setImageUrl(e.target.value)} className='border-2 m-1 px-5 py-2 w-[45%] rounded' type="text" placeholder='Image URL' />
+        <input value={userRole} onChange={(e)=> setUserRole(e.target.value)} className='border-2 m-1 px-5 py-2 w-[45%] rounded' type="text" placeholder='Enter role' />
+        <input value={userDesc} onChange={(e)=> setUserDesc(e.target.value)} className='border-2 m-1 px-5 py-2 w-[45%] rounded' type="text" placeholder='Enter Description' />
+        <button className='basis-[90%] px-3 py-3 bg-[#4F46E5] active:scale-[.9] text-white rounded-2xl'>Submit</button>
 
       </form>
    <div className='flex flex-wrap gap-2 p-3'>
--- Day-75(cardGenerator)/src/Component/Card.jsx
+++ Day-75(cardGenerator)/src/Component/Card.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
 const Card = (props) => {
-  console.log(props.id)
+  console.log(props)
   return (
     <div >
-    <div  className='p-4 rounded-2xl w-[280px] h-[320px] flex flex-col items-center bg-amber-300'>
-            <img className='w-20 h-20 object-top object-cover rounded-[50%]' src={props.elem.url} alt="" />
-            <h1 className='font-bold text-xl'>{props.elem.name}</h1>
-            <h2 className='font-semibold text-blue-500'>{props.elem.role}</h2>
-            <h3 className='text-center'>{props.elem.description}</h3>
-            <button onClick={()=>{props.deleteHandler(props.idx)}} className='bg-red-400 px-4 py-2 rounded'>Remove</button>
+    <div  className='p-4 rounded-2xl w-[280px] h-[320px] flex flex-col items-center justify-start  bg-[#242429]'>
+            <img className='w-20 h-20 object-top object-cover rounded-[50%]' src={props.elem.imageURL} alt="" />
+            <h1 className='font-bold text-xl'>{props.elem.userName}</h1>
+            <h2 className='font-semibold text-blue-500'>{props.elem.userRole}</h2>
+            <h3 className='text-center mt-2 text-[#E5E7EB]'>{props.elem.userDesc}</h3>
+            <button onClick={()=>{props.deleteHandler(props.idx)}} className='bg-red-400 text-white px-3 py-2  rounded-[8px] mt-2'>Remove</button>
           </div>
     </div>
   )
